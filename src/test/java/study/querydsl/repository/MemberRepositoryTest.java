3ad0e12c203ac41839b055441d1d8fae386d1106
steve <steve6133@naver.com>
2023-06-09 17:59:02 +0900

    feat : memberRepository 생성

---
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -0,0 +1,44 @@
+package study.querydsl.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import study.querydsl.entity.Member;
+
+import javax.transaction.Transactional;
+
+import java.util.List;
+
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void basicTest(){
+        Member member = new Member("member1", 10);
+        memberRepository.save(member);
+
+
+        Member findMember = memberRepository.findById(member.getId()).get();
+
+
+        Assertions.assertThat(findMember).isEqualTo(member);
+
+        List<Member> result1 = memberRepository.findAll();
+
+        Assertions.assertThat(result1).containsExactly(member);
+
+
+        List<Member> result2 = memberRepository.findByUsername("member1");
+
+        Assertions.assertThat(result2).containsExactly(member);
+
+    }
+
+}
